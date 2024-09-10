567bc4fcd9f34c87b90766d93a035283c268bf48
Mohitduaa <duamohit10@gmail.com>
2024-09-10 13:38:06 +0530

    path changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,8 @@
-import BudgetContact from "@/Components/BudgetContact";
 import Card from "@/Components/Card";
 import Development_approach from "@/Components/Development_approach";
-import Digitalmarketing from "@/Components/Digitalmarketing";
-import Footer from "@/Components/Footer";
 import Great_software from "@/Components/Great_software";
 import Hero_section from "@/Components/Hero_section";
-import Navbar from "@/Components/Navbar";
 import Projects from "@/Components/Projects";
-import WebDevelopment from "@/Components/WebDevelopment";
-import Image from "next/image";
 export default function Home() {
   return (
     <main className="">
